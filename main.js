d561b02fed94bc3f1becb82967165bd544273a20
Isaac-03 <osuntokiisaac2018@gmail.com>
2022-07-13 13:48:02 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+console.log("Hello world");
+// alert("Hello world");
+const x = 2;
+    y = 3;
+    z = "Fav"
+
+console.log(x + y);
+console.log(typeof x);
+console.log(typeof z);
+
+// document.getElementById.para.style(color) = red;
+
+var char = "a";
+var obj = {name: "Isaac", age: "20"}
+var arr = ["Jesus " + "Is" + " Lord", 25, "Spirit"];
+var arr1 = [2,4,6,8,10];
+
+console.log(obj,0);
+
+console.log(char);
+console.log(obj);
+console.log(arr);
+console.log(arr1);
+
+console.log(typeof char);
+console.log(typeof obj);
+console.log(typeof arr);
+console.log(typeof arr1);
+
+const num1 = 2;
+const num2 = 4;
+console.log(num1 + num2);
+
+function num(num1, num2) {
+    console.log("Answer is", num1 + num2);
+};
+num(3, 6);
+
+// var colors = require('../safe');
+// console.log(colors.yellow.underline('Underline that text'));
